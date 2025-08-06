8aea2f15f66ed1ec39f3d0261f3100547cbcc176
christiandumanauw <christian.dumanauw@generali.com.hk>
2025-08-06 13:35:05 +0800

    set spinner logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { CommentBox } from "./components/CommentBox"
 import { CommentInputBox } from "./components/CommentInputBox"
 import type { CommentObject } from './model/CommentObject';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
   const [comments, setComments] = useState<CommentObject[]>([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -33,19 +33,27 @@ function App() {
 
   return (
     <div className="justify-center items-center flex flex-row w-screen min-h-screen bg-gray-900">
-      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full border">
+      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full">
       </div>
       <div className="comment-box w-3/5 max-lg:w-3/5 max-md:w-3/5 max-sm:w-full border">
         <CommentInputBox>
         </CommentInputBox>
         <hr className="w-full bg-white border-1"></hr>
         <div>
+          {comments.length === 0 &&
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          }
           {comments.map((comment: CommentObject) => (
-            <CommentBox key={comment.id} id={comment.id} username={comment.username} content={comment.content} created_at={comment.created_at} />
+            <div>
+              <CommentBox key={comment.id} id={comment.id} username={comment.username} content={comment.content} created_at={comment.created_at} />
+              <hr className="w-full bg-white border-1"></hr>
+            </div>
           ))}
         </div>
       </div>
-      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full border">
+      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full">
       </div>
     </div>
   )
