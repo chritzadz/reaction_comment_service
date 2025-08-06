95b5fe4b9c3bb3242036d01c4d097af628a9ddc4
christiandumanauw <christian.dumanauw@generali.com.hk>
2025-08-06 09:39:52 +0800

    seperated CommentBox and CommentBoxInput

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import { CommentBox } from "./components/CommentBox"
+import { CommentInputBox } from "./components/CommentInputBox"
 
 function App() {
   return (
-    <div className="justify-center items-center flex flex-row">
-      <div className="comment-box w-1/5 h-screen bg-gray-900 border">
-        hello
+    <div className="justify-center items-center flex flex-row w-screen min-h-screen bg-gray-900">
+      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full border">
       </div>
-      <div className="comment-box w-3/5 h-screen bg-gray-900 border">
-        <CommentBox>
-          
-        </CommentBox>
+      <div className="comment-box w-3/5 max-lg:w-3/5 max-md:w-3/5 max-sm:w-full border">
+        <CommentInputBox>
+        </CommentInputBox>
+        <hr className="w-full bg-white border-1"></hr>
+        <CommentBox></CommentBox>
       </div>
-      <div className="comment-box w-1/5 h-screen bg-gray-900 border">
-        hello
+      <div className="comment-box w-1/5 max-lg:w-1/5 max-md:w-1/5 max-sm:w-0 h-full border">
       </div>
     </div>
   )
--- frontend/src/components/CommentBox.tsx
+++ frontend/src/components/CommentBox.tsx
@@ -3,7 +3,7 @@ import heartIcon from '../assets/heart.png';
 import chatBubble from '../assets/chat-bubble.png';
 
 export function CommentBox() {
-    const [commentSectionValue, setCommentSectionValue] = useState("");
+    const [commentSectionValue, setCommentSectionValue] = useState("Hello, this is a comment!");
 
     const username: String = "christianDumanauw";
     const firstLetter: String = username[0].toUpperCase();
@@ -17,13 +17,8 @@ export function CommentBox() {
                 </div>
                 <p className="text-white text-xl justify-start">{username}</p>
             </div>
-            <div className="w-full"> {/* comment section */}
-                <textarea
-                    value={commentSectionValue}
-                    onChange={(e) => setCommentSectionValue(e.target.value)}
-                    placeholder='What are you thinking today!'
-                    className="w-full aspect-[9/3] border-white border-2 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-white"
-                />
+            <div className="w-full flex flex-col gap-3"> {/* comment section */}
+                <p className="w-full text-white rounded-md text-lg">{commentSectionValue}</p>
                 <div className="flex w-full text-md text-white"> {/* date */}
                     <p>{date}</p>
                 </div>
