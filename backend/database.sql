f21084fd8ebdfb2ec0855588f5b4629a70efc8f8
christiandumanauw <christian.dumanauw@generali.com.hk>
2025-08-07 11:09:03 +0800

    set up database definition

--- backend/database.sql
+++ backend/database.sql
@@ -1,8 +1,27 @@
 CREATE DATABASE rc_service;
 
-CREATE TABLE comments (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_at DATE NOT NULL
+);
+
+CREATE TABLE post_have_replies (
+    post_id INT NOT NULL,
+    reply_id INT NOT NULL,
+    PRIMARY KEY (post_id, reply_id)
+);
+
+CREATE TABLE replies (
+    id SERIAL PRIMARY KEY AUTO_INCREMENT,
+    post_id INT NOT NULL,
+    content TEXT NOT NULL,
+);
+
+CREATE TABLE reactions (
+    reply_id INT NOT NULL,
+    type ENUM('like', 'love', 'haha', 'sad', 'angry') NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    PRIMARY KEY(reply_id, username)
 );
